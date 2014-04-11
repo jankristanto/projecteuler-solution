8d460a50e3b63e0c177e11c07c794b19d8e9e065
Jan Kristanto <jan@ontelstudio.com>
2014-04-12 00:00:45 +0700

    problem1 solve

--- problem1/solutioncode.php
+++ problem1/solutioncode.php
@@ -1,3 +1,8 @@
-<?php 
+<?php 						
+function jumlahHabisDibagi($pembagi,$batas){
+	return $pembagi*floor($batas/$pembagi)*(floor($batas/$pembagi)+1)/2;
+}
 
+$sum = jumlahHabisDibagi(3,999) + jumlahHabisDibagi(5,999) - jumlahHabisDibagi(15,999);
+echo $sum;
 ?>
